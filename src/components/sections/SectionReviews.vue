9214d54f0d95c7b273eb3d19fa0fb322b09064cd
Hakan Camcioglu <mr.hakancamcioglu@gmail.com>
2024-05-01 17:34:34 +0300

    🔧Componentize home view (2/2)

---
+++ src/components/sections/SectionReviews.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import ReviewCard from '@/components/ReviewCard.vue'
+</script>
+
+<template>
+  <section class="reviews">
+    <h2>reviews</h2>
+    <h3>Read What Others Have To Say</h3>
+    <div class="review-container">
+      <ReviewCard author="Wheatley" src="src\assets\wheatley.png"
+        >I wish I could take it all back. I honestly do. I honestly do wish I could take it all
+        back. And not just 'cause I'm stranded in space.</ReviewCard
+      >
+      <ReviewCard author="Wheatley" src="src\assets\wheatley.png"
+        >I wish I could take it all back. I honestly do. I honestly do wish I could take it all
+        back. And not just 'cause I'm stranded in space.</ReviewCard
+      >
+      <ReviewCard author="Wheatley" src="src\assets\wheatley.png"
+        >I wish I could take it all back. I honestly do. I honestly do wish I could take it all
+        back. And not just 'cause I'm stranded in space.</ReviewCard
+      >
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.reviews {
+  text-align: center;
+}
+
+.review-container {
+  text-align: center;
+  @apply grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 lg:gap-6;
+}
+
+.review-container div {
+  @apply hover:scale-95 shadow-2xl hover:shadow-sm;
+}
+
+.reviews h3 {
+  font-family: 'Outfit';
+  @apply font-bold text-3xl xl:text-4xl col-span-3 col-start-2;
+}
+
+@media only screen and (min-width: 530px) and (max-width: 639px) {
+  .review-container {
+    @apply grid-cols-2 !important;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-import PillButton from '@/components/PillButton.vue'
-import ShapeEight from '@/components/shapes/ShapeEight.vue'
-import ShapeTwelve from '@/components/shapes/ShapeTwelve.vue'
-import ProjectCard from '@/components/ProjectCard.vue'
-import ReviewCard from '@/components/ReviewCard.vue'
 import SectionHero from '@/components/sections/SectionHero.vue'
-
-import { ref, onMounted, onUnmounted } from 'vue'
-
-import { ScrollComponent } from 'potiah'
 import SectionOne from '@/components/sections/SectionOne.vue'
 import SectionSkills from '@/components/sections/SectionSkills.vue'
+import SectionReviews from '@/components/sections/SectionReviews.vue'
+import SectionProjects from '@/components/sections/SectionProjects.vue'
+import SectionCallToAction from '@/components/sections/SectionCallToAction.vue'
+
+import { ref, onMounted, onUnmounted } from 'vue'
 
 var windowWidth = ref(window.innerWidth)
 
@@ -32,50 +28,8 @@ onUnmounted(() => {
     <SectionHero />
     <SectionOne />
     <SectionSkills />
-    <section class="highlighted-projects">
-      <h2>highlighted projects</h2>
-      <div class="projects-grid">
-        <ProjectCard title="Hello World" src="https://placehold.co/600x400"
-          >hello guys its me flying gorilla welcome back to my flying gorilla simulator</ProjectCard
-        >
-        <ProjectCard title="Hello World" src="https://placehold.co/600x400"
-          >hello guys its me flying gorilla welcome back to my flying gorilla simulator</ProjectCard
-        >
-        <ProjectCard title="Hello World" src="https://placehold.co/600x400"
-          >hello guys its me flying gorilla welcome back to my flying gorilla simulator</ProjectCard
-        >
-        <ProjectCard title="Hello World" src="https://placehold.co/600x400"
-          >hello guys its me flying gorilla welcome back to my flying gorilla simulator</ProjectCard
-        >
-      </div>
-    </section>
-    <section class="reviews">
-      <h2>reviews</h2>
-      <h3>Read What Others Have To Say</h3>
-      <div class="review-container">
-        <ReviewCard author="Wheatley" src="src\assets\wheatley.png"
-          >I wish I could take it all back. I honestly do. I honestly do wish I could take it all
-          back. And not just 'cause I'm stranded in space.</ReviewCard
-        >
-        <ReviewCard author="Wheatley" src="src\assets\wheatley.png"
-          >I wish I could take it all back. I honestly do. I honestly do wish I could take it all
-          back. And not just 'cause I'm stranded in space.</ReviewCard
-        >
-        <ReviewCard author="Wheatley" src="src\assets\wheatley.png"
-          >I wish I could take it all back. I honestly do. I honestly do wish I could take it all
-          back. And not just 'cause I'm stranded in space.</ReviewCard
-        >
-      </div>
-    </section>
-    <section class="call-to-action">
-      <div class="inner"></div>
-      <div class="center">
-        <div class="contact-card">
-          <p>ready to turn ideas into reality?</p>
-          <h2>Let's Stop Scrolling And Start Creating</h2>
-          <PillButton>contact me</PillButton>
-        </div>
-      </div>
-    </section>
+    <SectionReviews />
+    <SectionProjects />
+    <SectionCallToAction />
   </main>
 </template>
